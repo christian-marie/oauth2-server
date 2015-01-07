2490f4624f348ee9f8135c0e888dfafa9f552c34
Thomas Sutton <me@thomas-sutton.id.au>
2015-01-07 15:34:19 +1100

    Travis should just run test.sh

--- test.sh
+++ test.sh
@@ -2,8 +2,13 @@
 
 set -eu
 
-[ -d ".cabal-sandbox" ] || cabal sandbox init
+# Setup the sandbox, if required.
+if [ ! -d ".cabal-sandbox" ]; then
+        cabal sandbox init
+        cabal sandbox add-source oauth2-server*
+fi
 
+# Test each package.
 for pkg in oauth2-server*; do
         cd $pkg
         [ -f "cabal.sandbox.config" ] || cabal sandbox init --sandbox=../.cabal-sandbox/
