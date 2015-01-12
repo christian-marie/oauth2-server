635e752d5efbda2b070ca3c00605fb35ed610fa5
Timo von Holtz <tvh@anchor.com.au>
2015-01-12 17:42:02 +1100

    Implement crypto token store

--- oauth2-server/lib/Network/OAuth2/Server/CryptoTokenStore.hs
+++ oauth2-server/lib/Network/OAuth2/Server/CryptoTokenStore.hs
@@ -1,6 +1,8 @@
-module Network.OAuth2.Server.CrypoTokenStore where
+{-# LANGUAGE RecordWildCards #-}
+module Network.OAuth2.Server.CryptoTokenStore where
 
 import Control.Monad.IO.Class
+import qualified Data.Set as Set
 
 import Crypto.AnchorToken
 
@@ -17,8 +19,20 @@ cryptoTokenStore key = TokenStore
     }
 
 load
-    :: AnchorCryptoState Pair
+    :: MonadIO m
+    => AnchorCryptoState a
     -> Token
     -> m (Maybe TokenGrant)
-load key token =
-    error "not implemented"
+load key (Token token) = do
+    tok <- liftIO $ verifyToken key token
+    case tok of
+        Left _ -> return Nothing
+        Right AnchorToken{..} -> return $ Just TokenGrant
+            { grantTokenType = _tokenType
+            , grantAccessToken = Token token
+            , grantRefreshToken = Just $ Token token
+            , grantExpires = _tokenExpires
+            , grantUsername = _tokenUserName
+            , grantClientID = _tokenClientID
+            , grantScope = Scope $ Set.fromList _tokenScope
+            }
