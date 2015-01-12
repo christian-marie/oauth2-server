81eee187fe3ca7781a9a290b44c75248298f18ea
Thomas Sutton <me@thomas-sutton.id.au>
2015-01-12 11:32:50 +1100

    Demo: add a user/password for tests

--- oauth2-server-demo/src/Main.hs
+++ oauth2-server-demo/src/Main.hs
@@ -1,32 +1,54 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
 {-# LANGUAGE TemplateHaskell   #-}
 
 module Main where
 
+import Control.Applicative
 import Control.Lens
-import Data.Monoid
-import qualified Data.Text.IO as T
+import Control.Monad.IO.Class
+import Data.IORef
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as M
+import Data.Set (Set)
+import qualified Data.Set as S
+import Data.Text (Text)
 import Snap.Http.Server
 import Snap.Snaplet
 
-import Network.OAuth2.Server.Configuration
+import Network.OAuth2.Server
 import Network.OAuth2.Server.Snap
 
 -- * OAuth2 Server
 
-oauth2Conf :: OAuth2Server IO
-oauth2Conf = Configuration
-    { oauth2Store = store
-    , oauth2CheckCredentials = checkCredentials
+data State = State
+    { sTokens :: Map Token TokenGrant
+    , sCreds  :: Set (Text, Text)
     }
-  where
-    store = TokenStore
-        { tokenStoreSave = saveToken
-        , tokenStoreLoad = loadToken
+
+oauth2Conf :: IO (OAuth2Server IO)
+oauth2Conf = do
+    ref <- newIORef (State M.empty $ S.singleton ("user", "password"))
+    return Configuration
+        { oauth2CheckCredentials = checkCredentials ref
+        , oauth2Store = TokenStore
+            { tokenStoreSave = saveToken ref
+            , tokenStoreLoad = loadToken ref
+            }
         }
-    saveToken _token = return ()
-    loadToken _token = return Nothing
-    checkCredentials _creds = return True
+  where
+    saveToken ref grant = modifyIORef ref (put grant)
+      where
+        put g@TokenGrant{..} (State ts ss) =
+            let ts' = M.insert grantAccessToken g ts
+            in State ts' ss
+    loadToken ref token = (M.lookup token . sTokens) <$> readIORef ref
+    checkCredentials ref creds = check creds . sCreds <$> readIORef ref
+      where
+        check RequestPassword{..} s =
+            (requestUsername, requestPassword) `S.member` s
+        check RequestClient{..} s =
+            (requestClientIDReq, requestClientSecretReq) `S.member` s
 
 -- * Snap Application
 
@@ -44,5 +66,6 @@ main = do
 
 app :: SnapletInit App App
 app = makeSnaplet "oauth2-server-demo" "A demonstration OAuth2 server." Nothing $ do
-    o <- nestSnaplet "oauth2" oauth2 $ initOAuth2Server oauth2Conf
+    cnf <- liftIO oauth2Conf
+    o <- nestSnaplet "oauth2" oauth2 $ initOAuth2Server cnf
     return $ App o
