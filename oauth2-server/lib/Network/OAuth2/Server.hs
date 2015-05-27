edb8af4e9fb803139a444a2c36031b6973eb760a
Timo von Holtz <tvh@anchor.com.au>
2015-05-27 13:20:40 +1000

    Rip out bits not linked to oauth2 RFC.

--- oauth2-server/lib/Network/OAuth2/Server.hs
+++ oauth2-server/lib/Network/OAuth2/Server.hs
@@ -6,48 +6,17 @@
 module Network.OAuth2.Server (
     module X,
     createGrant,
-    checkToken,
-    anchorTokenTokenGrant,
 ) where
 
 import Control.Applicative
-import Control.Lens
 import Control.Monad
 import Control.Monad.IO.Class
 import Data.Maybe
 import Data.Monoid
-import qualified Data.Set as Set
-import Data.Text (Text)
 import Data.Time.Clock
-
-import Crypto.AnchorToken as Token
 
 import Network.OAuth2.Server.Configuration as X
 import Network.OAuth2.Server.Types as X
-
--- | Convert an 'AnchorToken' into a 'TokenGrant'.
-anchorTokenTokenGrant
-    :: AnchorCryptoState Pair
-    -> Iso' AnchorToken TokenGrant
-anchorTokenTokenGrant key = iso packitallup unpackitall
-  where
-    packitallup t@AnchorToken{..} =
-        let token = signToken key t
-        in TokenGrant
-            { grantTokenType = _tokenType
-            , grantToken = Token token
-            , grantExpires = _tokenExpires
-            , grantUsername = _tokenUserName
-            , grantClientID = _tokenClientID
-            , grantScope = Scope $ Set.fromList _tokenScope
-            }
-    unpackitall TokenGrant{..} = AnchorToken
-        { _tokenType = grantTokenType
-        , _tokenExpires = grantExpires
-        , _tokenUserName = grantUsername
-        , _tokenClientID = grantClientID
-        , _tokenScope = Set.toAscList . unScope $ grantScope
-        }
 
 -- | Create a 'TokenGrant' representing a new token.
 --
@@ -59,7 +28,7 @@ createGrant
     -> m (TokenGrant, TokenGrant)
 createGrant Configuration{..} request = do
     t <- liftIO getCurrentTime
-    (client, user, Scope scope) <- case request of
+    (client, user, scope) <- case request of
             RequestPassword{..} ->
                 return
                 ( requestClientID
@@ -81,43 +50,17 @@ createGrant Configuration{..} request = do
                     , fromMaybe mempty (requestScope <|> (grantScope <$> previous))
                     )
     let expires = addUTCTime 1800 t
-        access_token = AnchorToken
-            { _tokenType = "access_token"
-            , _tokenExpires = expires
-            , _tokenUserName = user
-            , _tokenClientID = client
-            , _tokenScope = Set.toAscList scope
+        access_grant = TokenGrant
+            { grantTokenType = "access_token"
+            , grantExpires = expires
+            , grantUsername = user
+            , grantClientID = client
+            , grantScope = scope
             }
-        access_grant = access_token ^. anchorTokenTokenGrant oauth2SigningKey
         -- Create a refresh token with these details.
         refresh_expires = addUTCTime (3600 * 24 * 7) t
-        refresh_token = access_token
-            { Token._tokenType = "refresh_token"
-            , Token._tokenExpires = refresh_expires
+        refresh_grant = access_grant
+            { grantTokenType = "refresh_token"
+            , grantExpires = refresh_expires
             }
-        refresh_grant = refresh_token ^. anchorTokenTokenGrant oauth2SigningKey
     return (access_grant, refresh_grant)
-
--- | Check if the 'Token' is valid.
-checkToken
-    :: Monad m
-    => OAuth2Server m
-    -> Token
-    -> Maybe Text
-    -> Maybe Text
-    -> Scope
-    -> m (Either String ())
-checkToken Configuration{..} token user client (Scope scope) = do
-    token' <- tokenStoreLoad oauth2Store token
-    return $ case token' of
-        Just TokenGrant{..} -> do
-            when (isJust grantUsername) $ do
-                unless (isJust user) $ fail "Username unspecified"
-                unless (user == grantUsername) $ fail "User incorrect"
-            when (isJust grantClientID) $ do
-                unless (isJust client) $ fail "ClientID unspecified"
-                unless (client == grantClientID) $ fail "ClientID incorrect"
-            let Scope scope' = grantScope
-            unless (scope `Set.isSubsetOf` scope') $
-                fail "Incorrect scope"
-        Nothing -> fail "Invalid Token"
