98d1dfcdb5d177e0c396a165be0fdf9d94693ab2
Thomas Sutton <me@thomas-sutton.id.au>
2015-06-09 10:59:24 +1000

    Comments describing tests to write

--- test/tokenserver.hs
+++ test/tokenserver.hs
@@ -1,4 +1,20 @@
+-- | Description: Test the server interface.
 module Main where
 
 main :: IO ()
-main = return ()
+main = fail "Not implemented"
+
+-- When a token is requested with a ClientID, it is granted with that ClientID.
+
+-- When a token is saved with a ClientID, that ClientID can validate it.
+
+-- When a token is saved with a ClientID, another ClientID cannot validate it.
+
+-- When a token is saved it is included in list of tokens for that user.
+
+-- When a token is revoked, it can no longer be verified.
+
+-- When a token is refreshed, the new token has the same details.
+
+-- When a token is refreshed, the old token is revoked. (Or expired? Or just
+-- deleted?)
---
+++ test/tokenstore.hs
@@ -0,0 +1,6 @@
+-- | Description: Test the token store functionality.
+module Main where
+
+main :: IO ()
+main = fail "Not implemented"
+
