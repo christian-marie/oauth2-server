d5268a6c92ec69dfc58e9056f5ffb9c906784e49
Tran Ma <tran@defma.in>
2015-06-09 16:00:03 +1000

    test: skeleton store test

--- test/tokenstore.hs
+++ test/tokenstore.hs
@@ -5,26 +5,47 @@ import           Control.Applicative
 import           Control.Lens.Operators
 import           Control.Monad
 import qualified Data.ByteString.Char8       as B
+import qualified Data.List                   as L
+import           Data.Maybe
 import qualified Data.Text                   as T
 import           Data.Time.Calendar
 import           Data.Time.Clock
 import           Network.OAuth2.Server
 import           Network.OAuth2.Server.Types
+import           Test.Hspec
+import           Test.Hspec.QuickCheck
 import           Test.QuickCheck
 
 
-instance Arbitrary Token where
-  arbitrary = do
-    s <- liftM (take 64) $ infiniteListOf $ elements ['a'..'z']
-    return (B.pack s ^?! token)
-
-instance Arbitrary TokenType where
-  arbitrary = arbitraryBoundedEnum
+alphabet = elements ['a'..'z']
 
 instance Arbitrary UTCTime where
   arbitrary = UTCTime <$> day <*> time
     where day = ModifiedJulianDay <$> arbitrary
           time = secondsToDiffTime <$> arbitrary
+
+instance Arbitrary Username where
+  arbitrary = do
+    s <- liftM (take 32) $ infiniteListOf alphabet
+    return (T.pack s ^?! username)
+
+instance Arbitrary Scope where
+  arbitrary = do
+    s <- liftM unwords $ listOf $ listOf alphabet
+    return $ fromJust $ bsToScope $ B.pack s -- trust me
+
+instance Arbitrary ClientID where
+  arbitrary = do
+    s <- liftM (take 32) $ infiniteListOf alphabet
+    return (B.pack s ^?! clientID)
+
+instance Arbitrary Token where
+  arbitrary = do
+    s <- liftM (take 64) $ infiniteListOf alphabet
+    return (B.pack s ^?! token)
+
+instance Arbitrary TokenType where
+  arbitrary = arbitraryBoundedEnum
 
 instance Arbitrary TokenGrant where
   arbitrary =   TokenGrant
@@ -34,12 +55,15 @@ instance Arbitrary TokenGrant where
             <*> arbitrary
             <*> arbitrary
 
-instance Arbitrary Username where
-  arbitrary = do
-    s <- liftM (take 32) $ infiniteListOf $ elements ['a'..'z']
-    return (T.pack s ^?! username)
+instance Arbitrary TokenDetails where
+  arbitrary = tokenDetails <$> arbitrary <*> arbitrary
+
+
+suite :: Spec
+suite = describe "Token Store" $ do
+  prop "can save and load a token" pending
 
 
 main :: IO ()
-main = fail "Not implemented"
+main = hspec suite
 
