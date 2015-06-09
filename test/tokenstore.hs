36305e52c87a32f461aa374040fc46c6aea082a4
Tran Ma <tran@defma.in>
2015-06-09 15:02:16 +1000

    test: some arbitrary types for testing store

--- test/tokenstore.hs
+++ test/tokenstore.hs
@@ -1,6 +1,45 @@
 -- | Description: Test the token store functionality.
 module Main where
 
+import           Control.Applicative
+import           Control.Lens.Operators
+import           Control.Monad
+import qualified Data.ByteString.Char8       as B
+import qualified Data.Text                   as T
+import           Data.Time.Calendar
+import           Data.Time.Clock
+import           Network.OAuth2.Server
+import           Network.OAuth2.Server.Types
+import           Test.QuickCheck
+
+
+instance Arbitrary Token where
+  arbitrary = do
+    s <- liftM (take 64) $ infiniteListOf $ elements ['a'..'z']
+    return (B.pack s ^?! token)
+
+instance Arbitrary TokenType where
+  arbitrary = arbitraryBoundedEnum
+
+instance Arbitrary UTCTime where
+  arbitrary = UTCTime <$> day <*> time
+    where day = ModifiedJulianDay <$> arbitrary
+          time = secondsToDiffTime <$> arbitrary
+
+instance Arbitrary TokenGrant where
+  arbitrary =   TokenGrant
+            <$> arbitrary
+            <*> arbitrary
+            <*> arbitrary
+            <*> arbitrary
+            <*> arbitrary
+
+instance Arbitrary Username where
+  arbitrary = do
+    s <- liftM (take 32) $ infiniteListOf $ elements ['a'..'z']
+    return (T.pack s ^?! username)
+
+
 main :: IO ()
 main = fail "Not implemented"
 
