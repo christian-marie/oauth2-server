165e1bc270e0c57cb75cf2c29240982c3155cf73
Tran Ma <tran@defma.in>
2015-06-10 14:39:27 +1000

    test: monadic quickcheck store test

--- test/tokenstore.hs
+++ test/tokenstore.hs
@@ -1,27 +1,36 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+
 -- | Description: Test the token store functionality.
 module Main where
 
 import           Control.Applicative
 import           Control.Lens.Operators
 import           Control.Monad
+import           Control.Monad.Error
+import           Control.Monad.Reader
 import qualified Data.ByteString.Char8       as B
 import qualified Data.List                   as L
 import           Data.Maybe
 import qualified Data.Text                   as T
 import           Data.Time.Calendar
 import           Data.Time.Clock
+import           Database.PostgreSQL.Simple
 import           Network.OAuth2.Server
 import           Network.OAuth2.Server.Types
 import           Test.Hspec
 import           Test.Hspec.QuickCheck
 import           Test.QuickCheck
+import           Test.QuickCheck.Monadic
+
+import           Anchor.Tokens.Server.Store
+import           Anchor.Tokens.Server.Types
 
 
 alphabet = elements ['a'..'z']
 
 instance Arbitrary UTCTime where
   arbitrary = UTCTime <$> day <*> time
-    where day = ModifiedJulianDay <$> arbitrary
+    where day  = ModifiedJulianDay <$> arbitrary
           time = secondsToDiffTime <$> arbitrary
 
 instance Arbitrary Username where
@@ -58,12 +67,32 @@ instance Arbitrary TokenGrant where
 instance Arbitrary TokenDetails where
   arbitrary = tokenDetails <$> arbitrary <*> arbitrary
 
+--------------------------------------------------------------------------------
 
-suite :: Spec
+type Test m = ErrorT OAuth2Error (ReaderT ServerState m)
+
+testStore :: m Connection
+testStore = undefined
+
+runTest :: Connection -> Test m a -> m a
+runTest = undefined
+
+cleanupStore :: Connection -> m ()
+cleanupStore = undefined
+
 suite = describe "Token Store" $ do
-  prop "can save and load a token" pending
+  it "can save then load a token" $ monadicIO $ do
+    store <- testStore
+    g  <- pick arbitrary
+    d  <- lift . runTest store . saveToken $ g
+    md <- lift . runTest store . loadToken $ tokenDetailsToken d
+    cleanupStore store
+    return $ Just d == md
 
+  prop "can list existing tokens" pending
+  prop "can revoke existing tokens" pending
+  prop "does nothing if token does not exist" pending
 
 main :: IO ()
-main = hspec suite
+main = undefined
 
