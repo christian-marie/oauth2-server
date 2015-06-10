2e27ce37b2757bef8d918a156207264ee583a757
Timo von Holtz <tvh@anchor.com.au>
2015-06-11 09:23:39 +1000

    Remove config-autoreloading

--- src/tokenserver.hs
+++ src/tokenserver.hs
@@ -4,7 +4,6 @@ module Main where
 import Control.Applicative
 import Control.Concurrent
 import Control.Concurrent.Async
-import Control.Concurrent.STM
 import Control.Exception
 import Control.Monad
 import Data.Configurator as C
@@ -23,36 +22,14 @@ main = do
             [] -> "/etc/anchor-token-server/anchor-token-server.conf"
             [fp] -> fp
             _ -> error "Usage: anchor-token-server [CONFIG]"
-    (conf', _) <- autoReloadGroups autoConfig [("main.",Required confFile)]
-    let conf = subconfig "main" conf'
+    conf <- load [Required confFile]
     loglevel <- maybe WARNING read <$> C.lookup conf "log-level"
     updateGlobalLogger rootLoggerName (setLevel loglevel)
     opts <- loadOptions conf
-    srv' <- startServer opts
-    active <- newTVarIO True
-    srv <- newTMVarIO srv'
-    subscribe conf' (prefix "main") $ \_ _ -> restartServer active srv conf
+    srv <- startServer opts
     finally waitForever $ do
-        debugM logName "Shutting down"
-        ss <- atomically $ do
-            writeTVar active False
-            takeTMVar srv
-        thread <- stopServer ss
+        thread <- stopServer srv
         wait thread
   where
-    restartServer active srv conf = do
-        (act,srv') <- atomically $ do
-            act <- readTVar active
-            srv' <- takeTMVar srv
-            return $ (act,srv')
-        when act $ do
-            loglevel <- maybe WARNING read <$> C.lookup conf "log-level"
-            updateGlobalLogger rootLoggerName (setLevel loglevel)
-            debugM logName "Reloading config"
-            opts <- loadOptions conf
-            thread <- stopServer srv'
-            newSrv <- startServer opts
-            wait thread
-            atomically $ putTMVar srv newSrv
     waitForever :: IO a
     waitForever = forever $ threadDelay maxBound
