e83eca599b1aab6f91fe11980a4b10b7689d1e87
Timo von Holtz <tvh@anchor.com.au>
2015-06-10 14:35:57 +1000

    Don't pretend restart is finished until thread has exited

--- src/tokenserver.hs
+++ src/tokenserver.hs
@@ -5,7 +5,6 @@ import Control.Applicative
 import Control.Concurrent
 import Control.Concurrent.Async
 import Control.Concurrent.STM
-import Control.Concurrent.STM.TSem
 import Control.Exception
 import Control.Monad
 import Data.Configurator as C
@@ -26,39 +25,32 @@ main = do
             _ -> error "Usage: anchor-token-server [CONFIG]"
     (conf', _) <- autoReloadGroups autoConfig [("main.",Required confFile)]
     let conf = subconfig "main" conf'
+    opts <- loadOptions conf
+    srv' <- startServer opts
     active <- newTVarIO True
-    srv <- newEmptyTMVarIO
-    lock <- atomically $ newTSem 1
-    restartServer active lock srv conf
-    subscribe conf' (prefix "main") $ \_ _ -> restartServer active lock srv conf
+    srv <- newTMVarIO srv'
+    subscribe conf' (prefix "main") $ \_ _ -> restartServer active srv conf
     finally waitForever $ do
         debugM logName "Shutting down"
-        srv' <- atomically $ do
+        ss <- atomically $ do
             writeTVar active False
-            tryTakeTMVar srv
-        case srv' of
-            Nothing -> return ()
-            Just ss -> do
-                thread <- stopServer ss
-                wait thread
+            takeTMVar srv
+        thread <- stopServer ss
+        wait thread
   where
-    restartServer active lock srv conf = do
+    restartServer active srv conf = do
         (act,srv') <- atomically $ do
-            waitTSem lock
             act <- readTVar active
-            srv' <- tryTakeTMVar srv
+            srv' <- takeTMVar srv
             return $ (act,srv')
         when act $ do
             loglevel <- maybe WARNING read <$> C.lookup conf "log-level"
             updateGlobalLogger rootLoggerName (setLevel loglevel)
             debugM logName "Reloading config"
             opts <- loadOptions conf
-            case srv' of
-               Nothing -> return ()
-               Just ss -> void $ stopServer ss
+            thread <- stopServer srv'
             newSrv <- startServer opts
-            atomically $ do
-                putTMVar srv newSrv
-                signalTSem lock
+            wait thread
+            atomically $ putTMVar srv newSrv
     waitForever :: IO a
     waitForever = forever $ threadDelay maxBound
