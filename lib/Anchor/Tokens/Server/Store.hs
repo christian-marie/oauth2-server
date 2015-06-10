29169a53e67a99456c19d8f893d8fad1dae9fbbc
Thomas Sutton <me@thomas-sutton.id.au>
2015-06-10 12:37:53 +1000

    FromRow with lensing

--- lib/Anchor/Tokens/Server/Store.hs
+++ lib/Anchor/Tokens/Server/Store.hs
@@ -1,18 +1,34 @@
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE MultiWayIf            #-}
 {-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE RankNTypes            #-}
 {-# LANGUAGE RecordWildCards       #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TemplateHaskell       #-}
 
 module Anchor.Tokens.Server.Store where
 
+import           Control.Applicative
+import           Control.Lens                               (Prism', preview)
+import           Control.Lens.Review
 import           Control.Monad.Error.Class
 import           Control.Monad.IO.Class
 import           Control.Monad.Reader.Class
 import           Control.Monad.Trans.Control
-import           Data.ByteString                    (ByteString)
+import           Data.ByteString                            (ByteString)
 import           Data.Monoid
 import           Data.Pool
+import           Data.Text                                  (Text)
+import           Data.Typeable
 import           Database.PostgreSQL.Simple
+import           Database.PostgreSQL.Simple
+import           Database.PostgreSQL.Simple.FromField
+import           Database.PostgreSQL.Simple.FromRow
+import           Database.PostgreSQL.Simple.ToField
+import           Database.PostgreSQL.Simple.ToRow
+import           Database.PostgreSQL.Simple.TypeInfo.Macro
+import qualified Database.PostgreSQL.Simple.TypeInfo.Static as TI
 import           Network.OAuth2.Server
 import           System.Log.Logger
 
@@ -62,3 +78,87 @@ checkCredentials _auth _req = do
     withResource pool $ \_conn -> do
         liftIO . debugM logName $ "Checking some credentials"
         fail "Nope"
+
+-- * User Interface operations
+
+-- | List the tokens for a user.
+--
+-- Returns a list of at most @page-size@ tokens along with the total number of
+-- pages.
+listTokens
+    :: ( MonadIO m
+       , MonadBaseControl IO m
+       , MonadError OAuth2Error m
+       , MonadReader ServerState m
+       )
+    => UserID
+    -> Page
+    -> m ([TokenDetails], Page)
+listTokens uid (Page p) = do
+    pool <- asks serverPGConnPool
+    Page size <- (optUIPageSize <$> asks serverOpts)
+    withResource pool $ \conn -> do
+        liftIO . debugM logName $ "Listing tokens for " <> show uid
+        tokens <- liftIO $ query conn "SELECT * FROM tokens WHERE (user_id = ?) LIMIT ? OFFSET ?" (uid, size, (p - 1) * size)
+        [Only pages] <- liftIO $ query conn "SELECT count(*) FROM tokens WHERE (user_id = ?)" (Only uid)
+        return (tokens, Page pages)
+
+revokeToken
+    :: ( MonadIO m
+       , MonadBaseControl IO m
+       , MonadError OAuth2Error m
+       , MonadReader ServerState m
+       )
+    => Token
+    -> m ()
+revokeToken token = do
+    pool <- asks serverPGConnPool
+    withResource pool $ \_conn -> do
+        liftIO . debugM logName $ "Revoking token: " <> show token
+        -- UPDATE tokens SET revoked = NOW() WHERE (token = ?)
+        return ()
+
+-- * Support Code
+
+-- $ Here we implement support for, e.g., sorting oauth2-server types in
+-- PostgreSQL databases.
+
+instance ToField TokenType where
+    toField Bearer = toField ("bearer" :: Text)
+    toField Refresh = toField ("refresh" :: Text)
+
+instance FromField TokenType where
+    fromField f bs
+        | typeOid f /= $(inlineTypoid TI.varchar) = returnError Incompatible f ""
+        | bs == Nothing = returnError UnexpectedNull f ""
+        | bs == bearer  = pure Bearer
+        | bs == refresh = pure Refresh
+        | otherwise     = returnError ConversionFailed f ""
+      where
+        bearer = Just "bearer"
+        refresh = Just "refresh"
+
+instance ToRow TokenGrant where
+    toRow (TokenGrant ty ex uid cid sc) =
+        toRow (ty, ex, review username <$> uid, review clientID <$> cid, scopeToBs sc)
+
+instance FromRow TokenDetails where
+    fromRow = TokenDetails <$> field
+                           <*> (mebbeField (preview token))
+                           <*> field
+                           <*> (preview username <$> field)
+                           <*> (preview clientID <$> field)
+                           <*> (mebbeField bsToScope)
+
+-- | Get a PostgreSQL field using a parsing function.
+--
+-- Fails when given a NULL or if the parsing function fails.
+mebbeField
+    :: forall a b. (Typeable a, FromField b)
+    => (b -> Maybe a)
+    -> RowParser a
+mebbeField parse = fieldWith fld
+  where
+    fld :: Field -> Maybe ByteString -> Conversion a
+    fld f mbs = (parse <$> fromField f mbs) >>=
+        maybe (returnError ConversionFailed f "") return
