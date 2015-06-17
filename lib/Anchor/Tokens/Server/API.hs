38e8e4276063ff1f6813d8798cc5cec7e285d733
Timo von Holtz <tvh@anchor.com.au>
2015-06-17 13:55:11 +1000

    Use handleShib

--- lib/Anchor/Tokens/Server/API.hs
+++ lib/Anchor/Tokens/Server/API.hs
@@ -92,6 +92,7 @@ type AuthorizeEndpoint
 type AuthorizePost
     = "authorize"
     :> Header OAuthUserHeader UserID
+    :> Header OAuthUserScopeHeader Scope
     :> ReqBody '[FormUrlEncoded] Code
     :> Post '[HTML] ()
 
@@ -151,8 +152,8 @@ server :: ServerState -> Server AnchorOAuth2API
 server ServerState{..}
        = tokenEndpoint serverOAuth2Server
     :<|> error ""
-    :<|> authorizeEndpoint serverPGConnPool
-    :<|> authorizePost serverPGConnPool
+    :<|> handleShib (authorizeEndpoint serverPGConnPool)
+    :<|> handleShib (authorizePost serverPGConnPool)
     :<|> handleShib (serverListTokens serverPGConnPool (optUIPageSize serverOpts))
     :<|> handleShib (serverDisplayToken serverPGConnPool)
     :<|> serverPostToken serverPGConnPool
@@ -161,17 +162,12 @@ server ServerState{..}
 -- and any other case is an internal error. handleShib consolidates
 -- checking these headers.
 handleShib
-    :: ( MonadIO m
-       , MonadBaseControl IO m
-       , MonadError ServantErr m
-       )
-    => (UserID -> Scope -> a -> m b)
+    :: (UserID -> Scope -> a)
     -> Maybe UserID
     -> Maybe Scope
     -> a
-    -> m b
 handleShib f (Just u) (Just s) = f u s
-handleShib _ _        _        = const $ throwError err500
+handleShib _ _        _        = error "Expexted Shibbloleth headers"
 
 authorizeEndpoint
     :: ( MonadIO m
@@ -179,31 +175,25 @@ authorizeEndpoint
        , MonadError ServantErr m
        )
     => Pool Connection
-    -> Maybe UserID
-    -> Maybe Scope
+    -> UserID
+    -> Scope
     -> Maybe ResponseTypeCode
     -> Maybe ClientID
     -> Maybe URI
     -> Maybe Scope
     -> Maybe ClientState
     -> m Html
-authorizeEndpoint pool u' permissions' rt c_id' redirect sc' st = do
+authorizeEndpoint pool user_id permissions rt c_id' redirect sc' st = do
     case rt of
         Nothing -> error "NOOOO"
         Just ResponseTypeCode -> return ()
-    u_id <- case u' of
-        Nothing -> error "NOOOO"
-        Just u_id -> return u_id
-    permissions <- case permissions' of
-        Nothing -> error "NOOOO"
-        Just permissions -> return permissions
     sc <- case sc' of
         Nothing -> error "NOOOO"
         Just sc -> if sc `compatibleScope` permissions then return sc else error "NOOOOO"
     c_id <- case c_id' of
         Nothing -> error "NOOOO"
         Just c_id -> return c_id
-    request_code <- runReaderT (createCode u_id c_id redirect sc st) pool
+    request_code <- runReaderT (createCode user_id c_id redirect sc st) pool
     return $ renderAuthorizePage request_code
 
 authorizePost
@@ -212,14 +202,12 @@ authorizePost
        , MonadError ServantErr m
        )
     => Pool Connection
-    -> Maybe UserID
+    -> UserID
+    -> Scope
     -> Code
     -> m ()
-authorizePost pool u code' = do
-    u_id <- case u of
-        Nothing -> error "NOOOO"
-        Just u_id -> return u_id
-    res <- runReaderT (activateCode code' u_id) pool
+authorizePost pool user_id _scope code' = do
+    res <- runReaderT (activateCode code' user_id) pool
     case res of
         Nothing -> error "NOOOO"
         Just uri -> do
