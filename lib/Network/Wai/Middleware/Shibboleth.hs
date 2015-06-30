19ccd4312c6135c2f88abd67bf6feb3153261347
Thomas Sutton <me@thomas-sutton.id.au>
2015-06-30 11:59:58 +1000

    Better parser for custom CIDR

--- lib/Network/Wai/Middleware/Shibboleth.hs
+++ lib/Network/Wai/Middleware/Shibboleth.hs
@@ -15,7 +15,8 @@ module Network.Wai.Middleware.Shibboleth where
 
 import           Data.Bits
 import qualified Data.ByteString      as BS
-import           Data.CaseInsensitive
+import qualified Data.CaseInsensitive as CI
+import           Data.IP
 import           Data.Monoid
 import           Data.Text            (Text)
 import qualified Data.Text            as T
@@ -44,10 +45,11 @@ defaultConfig =
 
 -- | Strip Shibboleth headers unless from a trusted upstream.
 shibboleth :: ShibConfig -> Middleware
-shibboleth ShibConfig{..} app req =
+shibboleth ShibConfig{..} app req respond =
+    print (upstream, remoteHost req) >>
     if req `fromUpstream` upstream
-        then app req
-        else app (filterHeaders prefix req)
+        then app req respond
+        else app (filterHeaders prefix req) respond
 
 -- | Inspect a request to determine whether it originated from a trusted
 --   upstream address.
@@ -60,7 +62,7 @@ filterHeaders
     -> Request
     -> Request
 filterHeaders pre req =
-    let check (n, _) = (foldedCase pre) `BS.isPrefixOf` (foldedCase n)
+    let check (n, _) = (CI.foldedCase pre) `BS.isPrefixOf` (CI.foldedCase n)
         rHeaders = filter check $ requestHeaders req
     in req { requestHeaders = rHeaders }
 
@@ -76,6 +78,7 @@ data CIDR
     | CIDR6 { cidr6Addr :: (Word32, Word32, Word32, Word32)
             , cidrMask  :: Word8
             }
+  deriving (Show, Eq)
 
 -- | Check whether the remote end of a socket is contained in a CIDR class.
 --
@@ -113,32 +116,34 @@ mask6 (w1, w2, w3, w4) mask =
         then (32, m - 32)
         else (fromIntegral m, 0)
 
+-- | Parse a CIDR expression into a 'CIDR' value.
+--
+--   We expect an IP address and a mask length separated by a slash.
 parseCIDR :: Text -> Either String CIDR
-parseCIDR s = case parse4 s of
-    Left _ -> parse6 s
-    Right v -> Right v
+parseCIDR s = case T.splitOn "/" s of
+    [a, m] -> do
+        mask <- parseMask m
+        addr <- parseAddr a
+        return $ addr mask
+    _ -> fail "CIDR expression should look like: addr/mask"
   where
     r :: Read a => Text -> Maybe a
     r = readMaybe . T.unpack
-    parse4 :: Text -> Either String CIDR
-    parse4 s = do
-        [addr, mask] <- return (T.splitOn "/" s)
-        n <- case r mask of
-            Nothing -> fail (T.unpack $ "Could not parse as mask: " <> mask)
-            Just v -> return v
-        [q1, q2, q3, q4] <- case (T.splitOn "." addr) of
-            v@[_,_,_,_] -> return v
-            _ -> Left (T.unpack $ "IPv4 address in dotted quad form please: " <> addr)
-        ip <- case (r q1, r q2, r q3, r q4) of
-            (Nothing, _, _, _) -> fail (T.unpack $ "Could not read: " <> q1)
-            (_, Nothing, _, _) -> fail (T.unpack $ "Could not read: " <> q2)
-            (_, _, Nothing, _) -> fail (T.unpack $ "Could not read: " <> q3)
-            (_, _, _, Nothing) -> fail (T.unpack $ "Could not read: " <> q4)
-            (Just a, Just b, Just c, Just d) ->
-                return ( (a `shiftL` 24)
+    parseMask s = case r s of
+        Nothing -> Left "Cannot parse mask"
+        Just m -> return m
+    parseAddr s = case parse4 s of
+        Right v -> return v
+        Left  e -> parse6 s
+    parse4 :: Text -> Either String (Word8 -> CIDR)
+    parse4 s =
+        case mapM r (T.splitOn "." s) of
+            Just [a, b, c, d] ->
+                return $ CIDR4 ( (a `shiftL` 24)
                        + (b `shiftL` 16)
                        + (c `shiftL` 8)
                        + d
                        )
-        return (CIDR4 ip n)
-    parse6 _ = Left "IPv6 addresses are currently unsupported."
+            _ -> Left (T.unpack $ "Could not read IPv4 address: " <> s)
+    parse6 s =
+        Left "IPv6 addresses are currently unsupported."
